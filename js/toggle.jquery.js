cdb2b9ac66bfc3d9d254fb248c57cc7bbd2e0146
jalbertbowden <jalbertbowden@gmail.com>
2016-04-08 14:54:41 -0400

    toggle ajax iframes so pageload isn't painful

--- js/toggle.jquery.js
+++ js/toggle.jquery.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
     $(this).removeClass("trigger-active");
   });
   $(".trigger").click(function(){
+    var iframeSource = $(this).attr("data-url");
     $(this).next(".embedded-content").slideToggle("slow,");
+    $(this).next(".embedded-content").find("iframe").attr("src", (iframeSource));
     $(this).next(".embedded-content:first-child").slideToggle("slow,");
   });
 });
