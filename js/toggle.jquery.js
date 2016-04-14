91d64ac5521195efa81491916eab57455a22aee9
jalbertbowden <jalbertbowden@gmail.com>
2016-04-14 12:30:58 -0400

    commented out jquery for injecting iframes on iframe ref anchor click....its not needed anymore

--- js/toggle.jquery.js
+++ js/toggle.jquery.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
 
   $(".trigger").click(function(){
     // get value of .trigger's data-url attribute
-    var iframeSource = $(this).attr("data-url");
+    // var iframeSource = $(this).attr("data-url");
     // add iframe element into embedded-content container
     // $(".embedded-content").append("<iframe class=\"iframed-content\" src=\"\"></iframe>");
-    $(this).next(".embedded-content").append("<iframe class=\"ref-doc\" src=\"\"></iframe>");
+    // $(this).next(".embedded-content").append("<iframe class=\"ref-doc\" src=\"\"></iframe>");
     // find next .embedded-content element's child iframe element's src attribute and append .trigger's data-url attribute value
-    $(this).next(".embedded-content").find("iframe").attr("src", (iframeSource));
+    // $(this).next(".embedded-content").find("iframe").attr("src", (iframeSource));
     // slidetoggle .embedded-content following .trigger
     $(this).next(".embedded-content").slideToggle("slow,");
     // $(this).next(".embedded-content:first-child").slideToggle("slow,");
