b1a3d23c3ab749bcb0a30b616757eda67c0ae4a3
jalbertbowden <jalbertbowden@gmail.com>
2016-04-23 19:01:33 -0400

    mobile navigation now responds (closes) on navigation anchor element link click

--- js/wwc-site.js
+++ js/wwc-site.js
@@ -11,8 +11,13 @@ $(document).ready(function()
     }
 
 	// mobile nav
+  // expand mobile nav on click of hamburger menu icon
 	$('#mobiletoggle').click(function() {
 		$("nav").toggleClass('expanded').siblings('ul').slideToggle();
 	});
+  // close mobile nav on click of any link in the mobile nav
+  $('nav a').click(function() {
+    $("nav").toggleClass('expanded').siblings('ul').slideToggle();
+  });
 
 });
