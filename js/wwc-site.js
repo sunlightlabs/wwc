c1b399d00f6219bc3f4467c883bb8c3251be90a5
jalbertbowden <jalbertbowden@gmail.com>
2016-06-01 18:39:49 -0400

    toggle functionality removed. new images for arrows added. styles redone for embedded content

--- js/wwc-site.js
+++ js/wwc-site.js
@@ -2,7 +2,8 @@
 $(document).ready(function() 
 {
 	// load scripts after pageload
-    var scripts = ["js/jquery.leaflet.wwc.js", "js/jquery.jfeed.pack.js", "js/display-rss.jquery.js", "js/toggle.jquery.js", "js/twitterFetcher.js", "js/twitterFetchertweets.js"];
+    // var scripts = ["js/jquery.leaflet.wwc.js", "js/jquery.jfeed.pack.js", "js/display-rss.jquery.js", "js/toggle.jquery.js", "js/twitterFetcher.js", "js/twitterFetchertweets.js"];
+    var scripts = ["js/jquery.leaflet.wwc.js", "js/jquery.jfeed.pack.js", "js/display-rss.jquery.js", "js/twitterFetcher.js", "js/twitterFetchertweets.js"];
     var scriptsTotal = scripts.length;
     for (var i = 0; i < scriptsTotal; i++){
       var s = document.createElement("script");
