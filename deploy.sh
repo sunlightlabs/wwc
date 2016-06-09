627807547dab45d8e89d79aa840f70ec059f83b4
crdunwel <clayton.dunwell@gmail.com>
2016-06-09 14:33:44 -0400

    deploy script for deploying to s3

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+s3cmd sync css s3://whatworkscities.sunlightfoundation.com/ --recursive
+s3cmd sync js s3://whatworkscities.sunlightfoundation.com/ --recursive
+s3cmd sync img s3://whatworkscities.sunlightfoundation.com/ --recursive
+s3cmd sync humans.txt s3://whatworkscities.sunlightfoundation.com/
+s3cmd sync index.html s3://whatworkscities.sunlightfoundation.com/
