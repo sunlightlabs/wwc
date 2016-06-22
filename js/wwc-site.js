513fbe6aa149b2e9c192f95ecff8fd93df669a8d
Albert Bowden <albert@sunlightfoundation.com>
2016-06-21 22:38:13 -0400

    rel noopener attribute for security. style sheet pruning

--- js/wwc-site.js
+++ js/wwc-site.js
@@ -1,9 +1,7 @@
-// Use any event to append the code
+// use any event to append the code
 $(document).ready(function() 
 {
 	// load scripts after pageload
-    // var scripts = ["js/jquery.leaflet.wwc.js", "js/jquery.jfeed.pack.js", "js/display-rss.jquery.js", "js/toggle.jquery.js", "js/twitterFetcher.js", "js/twitterFetchertweets.js"];
-    // var scripts = ["js/jquery.leaflet.wwc.js", "js/jquery.jfeed.pack.js", "js/display-rss.jquery.js", "js/twitterFetcher.js", "js/twitterFetchertweets.js"];
     var scripts = ["js/jquery.leaflet.wwc.js", "js/jquery.jfeed.pack.js", "js/jquery.display-rss.js", "js/twitterFetcher.js", "js/twitterFetchertweets.js"];
     var scriptsTotal = scripts.length;
     for (var i = 0; i < scriptsTotal; i++){
@@ -11,7 +9,6 @@ $(document).ready(function()
       s.src = scripts[i];
       $("head").append(s);
     }
-
     // change icon image aka color on hover - using jquery bc rwd is not playing nice aka i suck
     $(".ref").hover(
       function () {
@@ -21,9 +18,9 @@ $(document).ready(function()
         $(this).find('.ref-link').attr('src', '../img/arrow-right-redx020.png');
       }
     );
-
-	// mobile nav
-  // expand mobile nav on click of hamburger menu icon
+    // swap out image for :visited style
+      $("a.ref:visited .ref-link").attr('src', '../img/arrow-right-dark-redx020.png');
+	// mobile nav - expand mobile nav on click of hamburger menu icon
 	$('#mobiletoggle').click(function() {
 		$("nav").toggleClass('expanded').siblings('ul').slideToggle();
 	});
@@ -31,5 +28,4 @@ $(document).ready(function()
   $('nav a').click(function() {
     $("nav").toggleClass('expanded').siblings('ul').slideToggle();
   });
-
 });
